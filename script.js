78c1b96656e5bbf5328ccd4256b610b2e6cfa345
Sycri <39947226+Sycri@users.noreply.github.com>
2023-05-24 09:28:23 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const $buttonLangSwitch = document.querySelector('.button-lang-switch');
+
+	if ($buttonLangSwitch) {
+		$buttonLangSwitch.parentElement.style.backgroundColor = 'transparent';
+	}
+});
